18e7f9a07948002f676d0ceaf5e97001bbb4c575
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-18 21:22:39 -0700

    navigating-to-dynamic-routes

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,20 @@
+import Link from 'next/link'
+
 function UserPage() {
+    const users = [
+        { id: 1, username: 'Masood' },
+        { id: 2, username: 'Admin' }
+    ]
     return (
         <div>
             <h2>User Page</h2>
+            <ul>
+                {users.map((item) => (
+                    <li key={item.id}>
+                        <Link href={`/users/${item.id}`}>{item.username}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
