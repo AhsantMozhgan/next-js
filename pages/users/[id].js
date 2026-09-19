9693aa6c801361cff9c2ac5c0eabab4e99ad41e7
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-18 21:46:01 -0700

    navigating-programmatically

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -5,9 +5,19 @@ function UserId() {
     console.log(router.pathname)
     console.log(router.query)
 
+    function showUserIdHandler() {
+        // router.push('/users/masood')
+        router.push({
+            pathname: '/users/[id]',
+            query: { id: 'masood' }
+        })
+
+    }
+
     return (
         <div>
         	<h2>User ID Page</h2>
+            <button onClick={showUserIdHandler}>Show User ID</button>
         </div>
     )
 }
--- pages/users/index.js
+++ pages/users/index.js
@@ -11,7 +11,9 @@ function UserPage() {
             <ul>
                 {users.map((item) => (
                     <li key={item.id}>
-                        <Link href={`/users/${item.id}`}>{item.username}</Link>
+                        <Link href={{pathname: '/users/[id]', query: { id: item.id }}}>
+                            {item.username}
+                        </Link>
                     </li>
                 ))}
             </ul>
