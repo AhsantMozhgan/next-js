7a903835f8bbd502b210f3dab0cf89546465e30b
AhsantMozhgan <mozhganahsant@gmail.com>
2026-09-19 10:48:06 -0700

    adding-getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import fs from 'fs/promises'
+import path from 'path'
+
 function HomePage({ products }) {
     return (
         <div>
@@ -12,9 +15,14 @@ function HomePage({ products }) {
 }
 
 export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), 'data', 'products.json')
+
+    const jsonData = await fs.readFile(filePath)
+    const data = JSON.parse(jsonData)
+
     return {
         props: {
-            products: [{ id: 'p1', title: 'Product 1' }],
+            products: data.products,
         },
     }
 }
